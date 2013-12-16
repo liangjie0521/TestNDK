80513814a499b9f50cfce5c1bb8f3d33672c1661
witmob <1032860079@qq.com>
2013-12-16 11:54:06 +0800

    删除不必要

--- Debug/jni/Utils.d
+++
@@ -1,6 +0,0 @@
-jni/Utils.d: ../jni/Utils.cpp /usr/lib/jvm/java-7-oracle/include/jni.h \
- /usr/lib/jvm/java-7-oracle/include/linux/jni_md.h
-
-/usr/lib/jvm/java-7-oracle/include/jni.h:
-
-/usr/lib/jvm/java-7-oracle/include/linux/jni_md.h:
--- gen/liangjie/app/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package liangjie.app;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
